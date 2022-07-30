99cec7f79ac0783e5b8cb6c52016a63a04145f62
JavohirXoldorov <javohirxoldorov7@gmail.com>
2022-07-30 06:42:50 +0500

    testing

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int newNumber = 0, k = 0;
+        while (number > 0) {
+            int r = number % 10;
+            newNumber *= 10;
+            newNumber += r;
+            number /= 10;
+        }
+        number = newNumber;
+        System.out.println(number);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public void toFahrenheit(int temperatureCelsius) {
+        double fahrenheit = temperatureCelsius * 1.8 + 32.0;
+        System.out.println(fahrenheit);
     }
 }
